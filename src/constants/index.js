7f161d19ff6607669ebfd2d95066f4eb85ed0471
mraihanhatim <raihan04042001@gmail.com>
2024-11-17 11:21:46 +0700

    create testimonials

--- src/constants/index.js
+++ src/constants/index.js
@@ -63,8 +63,8 @@ export const feedback = [
     id: "feedback-1",
     content:
       "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
-    name: "Herman Jensen",
-    title: "Founder & Leader",
+    name: "Raihan Hatim",
+    title: "CEO",
     img: people01,
   },
   {
@@ -72,15 +72,15 @@ export const feedback = [
     content:
       "Money makes your life easier. If you're lucky to have it, you're lucky.",
     name: "Steve Mark",
-    title: "Founder & Leader",
+    title: "CTO",
     img: people02,
   },
   {
     id: "feedback-3",
     content:
       "It is usually people in the money business, finance, and international trade that are really rich.",
-    name: "Kenn Gallagher",
-    title: "Founder & Leader",
+    name: "Jensen Carlsen",
+    title: "Project Manager",
     img: people03,
   },
 ];
