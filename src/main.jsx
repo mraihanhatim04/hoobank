277c4529082cfde6e9b27f2c6a0a3020990bf90e
mraihanhatim <raihan04042001@gmail.com>
2024-11-17 12:52:09 +0700

    speed insight

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <SpeedInsights>
+      <App />
+    </SpeedInsights>
+  </StrictMode>
+);
