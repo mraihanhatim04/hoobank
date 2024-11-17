fcb9ae62bd6e1e6b3d8760936326d829d05ca7fe
mraihanhatim <raihan04042001@gmail.com>
2024-11-17 12:54:18 +0700

    speed insight

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./style";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import {
   Navbar,
@@ -16,32 +17,35 @@ import {
 
 const App = () => {
   return (
-    <div className="bg-zinc-900 w-full overflow-hidden">
-      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>
-          <Navbar />
+    <>
+      <div className="bg-zinc-900 w-full overflow-hidden">
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Navbar />
+          </div>
         </div>
-      </div>
 
-      <div className={`bg-zinc-900 ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
-          <Hero />
+        <div className={`bg-zinc-900 ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Hero />
+          </div>
         </div>
-      </div>
 
-      <div className={`bg-zinc-900 ${styles.paddingX} ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
-          <Stats />
-          <Business />
-          <Billing />
-          <CardDeal />
-          <Testimonials />
-          <Clients />
-          <CTA />
-          <Footer />
+        <div className={`bg-zinc-900 ${styles.paddingX} ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Stats />
+            <Business />
+            <Billing />
+            <CardDeal />
+            <Testimonials />
+            <Clients />
+            <CTA />
+            <Footer />
+          </div>
         </div>
       </div>
-    </div>
+      <SpeedInsights />
+    </>
   );
 };
 
