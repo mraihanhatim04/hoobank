9453ea4dc458e53af99a24a6d51da3a795c3c398
mraihanhatim <raihan04042001@gmail.com>
2024-11-17 09:45:44 +0700

    create structure & layout folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
 import React from "react";
+import styles from "./style";
+
+import {
+  Navbar,
+  Billing,
+  Business,
+  CTA,
+  CardDeal,
+  Clients,
+  Footer,
+  Hero,
+  Stats,
+  Testimonials,
+} from "./components";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline flex h-screen justify-center items-center">
-        HiiBank
-      </h1>
+    <div className="bg-zinc-900 w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-zinc-900 ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-zinc-900 ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 };
