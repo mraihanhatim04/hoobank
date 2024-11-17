8b130a3a3995e1bba77e3acf6ae5250bebb836d0
mraihanhatim <raihan04042001@gmail.com>
2024-11-17 09:17:43 +0700

    starter project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline flex h-screen justify-center items-center">
+        HiiBank
+      </h1>
+    </div>
+  );
+};
+
+export default App;
